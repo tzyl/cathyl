3ca787ac8084d24d5da03b14e6137ed392f43f07
Timothy Leung <timothy.leung@balliol.ox.ac.uk>
2016-03-18 20:32:08 +0000

    Added instagram tiles from tutorial.

--- js/cathyl.js
+++ js/cathyl.js
@@ -14,6 +14,20 @@ $(document).ready(function() {
             scrollTop: $(hash).offset().top
         }, 1000, 'easeInOutCubic');
     });
+
+    // Set up instafeed.
+    var feed = new Instafeed({
+        clientId: '97ae5f4c024c4a91804f959f43f2635f',
+        target: 'instafeed',
+        get: 'tagged',
+        tagName: 'photographyportfolio',
+        links: true,
+        limit: 8,
+        sortBy: 'most-recent',
+        resolution: 'standard_resolution',
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 add-animation"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+    });
+    feed.run();
 });
 
 $(window).on('scroll', function() {
