6662d44a1e8991386874118c482f43203f274fe2
Timothy Leung <timothy.leung@balliol.ox.ac.uk>
2016-03-23 21:00:32 +0000

    Fixed links for social buttons.

--- js/cathyl.js
+++ js/cathyl.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('body').scrollspy({target: '.navbar', offset: 70});
 
     // Smooth scrolling when navbar link clicked.
-    $('nav a, a.scroll-arrow').on('click', function(e) {
+    $('.nav.navbar-nav.navbar-right a, a.scroll-arrow').on('click', function(e) {
         e.preventDefault();
         var hash = this.hash;
 
